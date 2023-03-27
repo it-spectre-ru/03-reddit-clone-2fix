444fbe24d5ae4a8ce2a1a30f1efe7527ac2a8124
Avv <it.spectre.ru@gmail.com>
2023-03-27 18:37:00 +0500

    setup icon

--- src/components/Navbar/Directory/Communities.tsx
+++ src/components/Navbar/Directory/Communities.tsx
@@ -1,5 +1,7 @@
+import { Flex, Icon, MenuItem } from '@chakra-ui/react';
 import React from 'react';
 import CreateCommunityModal from '../../Modal/CreateCommunity/CreateCommunityModal';
+import { GrAdd } from 'react-icons/gr';
 
 type CommunitiesProps = {};
 
@@ -7,6 +9,16 @@ const Communities: React.FC<CommunitiesProps> = () => {
   return (
     <>
       <CreateCommunityModal />
+      <MenuItem
+        width="100%"
+        fontSize="10pt"
+        _hover={{ bg: 'gray.100' }}
+        onClick={() => {}}>
+        <Flex align="center">
+          <Icon fontSize={20} mr={2} as={GrAdd} />
+          Create Community
+        </Flex>
+      </MenuItem>
     </>
   );
 };
